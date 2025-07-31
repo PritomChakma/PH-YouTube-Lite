31604d6c92271255ce12f44166d3ae83134193ce
Pritom Chakma <chakmapritom1@gmail.com>
2025-08-01 03:46:26 +0600

    show the videos by its category

--- script/category.js
+++ script/category.js
@@ -1,3 +1,4 @@
+// load category button
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
@@ -5,15 +6,50 @@ const loadCategories = () => {
     .catch((err) => console.log(err));
 };
 
+// show the all
 const showCategories = (categories) => {
   const catergory = document.getElementById("category-id");
   categories.forEach((element) => {
-    console.log(element);
     const button = document.createElement("button");
     button.classList = "btn";
     button.innerHTML = element.category;
     catergory.appendChild(button);
   });
 };
+// --------------------------------------------------------
+
+// load video
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => showVideos(data.videos))
+    .catch((err) => console.log(err));
+};
+
+const showVideos = (videos) => {
+  const videoId = document.getElementById("video");
+  videos.forEach((video) => {
+    console.log(video);
+    const videoContainer = document.createElement("div");
+    videoContainer.innerHTML = `
+    
+     <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    `;
+    videoId.appendChild(videoContainer)
+  });
+};
 
 loadCategories();
+loadVideos();
+
